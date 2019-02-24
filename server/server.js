3f5bc2cdf8b6c6119297b303d4cfe3c867789aae
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-02-24 12:53:53 +0530

    Added socket.io and basic connections

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const pub_path  = path.join(__dirname, '../public');
 
 app.use(express.static(pub_path));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{console.log(`Listening on PORT:${PORT}`)});
+io.on('connection', (socket)=>{
+    console.log("Hi there newbie!");
+
+    socket.on('disconnect', ()=>{
+        console.log("Bye bye newbie!");
+    })
+})
+
+
+
+server.listen(PORT, ()=>{console.log(`Listening on PORT:${PORT}`)});
 
 
