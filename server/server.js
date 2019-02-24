bf25e4a41a6334d851c97e5367bac26d7eaa3d10
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-02-24 10:08:12 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const pub_path  = path.join(__dirname, '../public');
+
+app.use(express.static(pub_path));
+
+const PORT=process.env.PORT;
+
+app.listen(PORT, ()=>{console.log(`Listening on PORT:${PORT}`)});
+
+
